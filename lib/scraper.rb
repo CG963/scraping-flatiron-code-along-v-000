01fdfc16f4303d7824b56dd81ced4ffa594b3eed
Christopher Garcia <christopher.garcia08@gmail.com>
2021-05-06 06:12:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,10 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogiri::HTML(open())
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
+  end
+  
   end
   
   def print_courses
@@ -22,5 +25,5 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page
 
